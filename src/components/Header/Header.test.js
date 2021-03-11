c5ee51621f0a95c4244f651d4cb7d2b8490b463e
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2021-03-11 21:08:12 +0000

    header test added

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,11 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Header from ".";
+
+it("renders correctly", () => {
+  const { getByText } = render(<Header />);
+
+  const title = getByText("AskBob - Plugins");
+
+  expect(title).toBeInTheDocument();
+});

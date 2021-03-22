410697d5baf4fd56771e4082198850f3b25694f1
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2021-03-22 22:03:42 +0000

    added query string search

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # askbob-skills-viewer
 
 A website for viewing the skills available on your AskBob program
+
+to deploy, enter npm start build. A build folder will then be made which you can
+you
--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header, Plugins, Categories, Selector } from "./components";
 import useFetchData from "./useFetchData";
 
 import { Typography } from "@material-ui/core";
 
 import useStyles from "./styles";
+import { useHistory } from "react-router-dom";
 
 const App = () => {
   const [sortBy, setSortBy] = useState("Plugins");
-  const [url, setUrl] = useState("");
+  const history = useHistory();
+  const [url, setUrl] = useState(history?.location?.search || "");
   const { plugins, skills, errorText, loading } = useFetchData(url);
 
   const classes = useStyles();
 
+  useEffect(() => {
+    const search = history?.location?.search || "";
+    console.log("search:", search);
+    const tokens = search.split("=");
+    console.log("tokens:", tokens);
+    console.log("tokens.length:", tokens.length);
+
+    const newUrl = tokens.length === 2 ? tokens[1] : "";
+    console.log("newUrl:", newUrl);
+
+    setUrl(newUrl);
+  }, [history]);
   if (loading) {
     return (
       <>
